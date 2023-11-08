fd8157e830688659989e63e580c295e4fdcf5011
UrviJoshi24 <113983709+UrviJoshi24@users.noreply.github.com>
2023-11-08 09:49:50 +0530

    Update serializers.py

--- backend-django/urvi/serializers.py
+++ backend-django/urvi/serializers.py
@@ -15,3 +15,13 @@ class PdfData(serializers.ModelSerializer):
     class Meta:
         model = budget
         fields = "__all__"
+
+class FinancialYearSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = financialyear
+        fields = '__all__'
+
+class PdfSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = pdf
+        fields = '__all__'  # Use this if you want to include all fields from the model
