cc116f09f306174363dd7250a38466532ca793b1
KaranJain09 <113978830+KaranJain09@users.noreply.github.com>
2024-02-25 18:27:05 +0530

    Update lab.sql

--- lab.sql
+++ lab.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `lab` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `lab`;
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: lab
 -- ------------------------------------------------------
--- Server version	8.0.30
+-- Server version	8.0.36
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -557,4 +557,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-25 16:13:39
+-- Dump completed on 2024-02-25 18:17:30
