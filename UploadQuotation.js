ecfd30bc3a2cc19f596d942d1a0c2b37a127bd77
swapniljoshi123 <141126193+swapniljoshi123@users.noreply.github.com>
2024-04-13 22:56:35 +0530

    Add files via upload

--- UploadQuotation.js
+++ UploadQuotation.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFolder } from '@fortawesome/free-solid-svg-icons';
+import { faFolder, faEllipsisV, faTrashAlt } from '@fortawesome/free-solid-svg-icons'; // Import the ellipsis-vertical and trash icons
 import 'reactjs-popup/dist/index.css'; // Import Popup styles
 import '../assets/css/UploadQuotation.css'; // Import CSS file for additional styling
 
@@ -10,6 +10,8 @@ function FolderCreator() {
   const [selectedOption, setSelectedOption] = useState('');
   const [folderName, setFolderName] = useState('');
   const [createdFolders, setCreatedFolders] = useState([]);
+  const [selectedFolder, setSelectedFolder] = useState(null);
+  const [editedFolderName, setEditedFolderName] = useState('');
 
   const handleOptionSelect = (option) => {
     if (option === 'newFolder') {
@@ -24,6 +26,24 @@ function FolderCreator() {
     setCreatedFolders([...createdFolders, folderName]);
     setFolderName(''); // Reset folder name input
     setShowPopup(false);
+  };
+
+  const handleFolderClick = (index) => {
+    setSelectedFolder(index);
+    setEditedFolderName(createdFolders[index]);
+  };
+
+  const handleFolderNameUpdate = (index) => {
+    const updatedFolders = [...createdFolders];
+    updatedFolders[index] = editedFolderName;
+    setCreatedFolders(updatedFolders);
+    setSelectedFolder(null);
+  };
+
+  const handleFolderDelete = (index) => {
+    const updatedFolders = [...createdFolders];
+    updatedFolders.splice(index, 1);
+    setCreatedFolders(updatedFolders);
   };
 
   const options = [{ value: 'newFolder', label: 'Create New Folder' }];
@@ -62,26 +82,33 @@ function FolderCreator() {
               value={folderName}
               onChange={(e) => setFolderName(e.target.value)}
             />
-            <input
-              type="text"
-              placeholder="sample"
-              value={folderName}
-              onChange={(e) => setFolderName(e.target.value)}
-            />
             <button onClick={handleCreateFolder}>Create Folder</button>
           </div>
         </Popup>
       )}
       {/* Display the new folder icons with folder names */}
-      <div className="folder-icons">
-        {createdFolders.map((folder, index) => (
-          <div key={index} className="folder-container">
-            <div className="new-folder-icon">
-              <FontAwesomeIcon icon={faFolder} size="2xl" />
-              <span className="folder-name">{folder}</span>
+      <div className='folder-area'>
+        <div className="folder-icons">
+          {createdFolders.map((folder, index) => (
+            <div key={index} className="folder-container">
+              <div className="new-folder-icon">
+                <FontAwesomeIcon icon={faFolder} size="2x" />
+                <FontAwesomeIcon icon={faEllipsisV} size="2x" className="ellipsis-icon" onClick={() => handleFolderDelete(index)} />
+                <span className="folder-name" onClick={() => handleFolderClick(index)}>{folder}</span>
+                {selectedFolder === index && (
+                  <input
+                    type="text"
+                    className="edit-folder-input"
+                    value={editedFolderName}
+                    onChange={(e) => setEditedFolderName(e.target.value)}
+                    onBlur={() => handleFolderNameUpdate(index)}
+                    autoFocus
+                  />
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
